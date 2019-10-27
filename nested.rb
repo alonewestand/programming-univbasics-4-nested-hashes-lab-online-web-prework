5be75f5ea87bccd8330cbff7542e5fbdf72d61a4
Ken Goad <goadke@g.cofc.edu>
2019-10-27 18:24:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:favorite_sport] = "tennis"
+programmer_hash[:dennis_ritchie][:languages]
 programmer_hash
 end
